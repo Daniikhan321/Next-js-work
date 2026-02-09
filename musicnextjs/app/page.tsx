e88b6490ea34e04d0a2b0dbce1fc66818f6a45a9
daniikhan <dk3906445@gmail.com>
2026-02-08 22:42:06 -0800

    navbar-changes

--- musicnextjs/app/page.tsx
+++ musicnextjs/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <h1 className="text-2xl text-center pt-3">
+ <main className="min-h-secreen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+   <h1 className="text-2xl text-center pt-3">
    Next Js project start
   </h1>
+ </main>
   );
 }
