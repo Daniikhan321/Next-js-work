519517d9c659f09192890ffb31bfc0a7d121b21a
daniikhan <dk3906445@gmail.com>
2026-02-09 23:16:23 -0800

    Herosection

--- musicnextjs/app/page.tsx
+++ musicnextjs/app/page.tsx
@@ -1,11 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
  <main className="min-h-secreen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-   <h1 className="text-2xl text-center pt-3">
-   Next Js project start
-  </h1>
+  <HeroSection/>
  </main>
   );
 }
